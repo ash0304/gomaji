23ce3c003dc3cb389cfc59fa4236ca2ebc555e95
Ash Lai <z0975201508@gmail.com>
2021-11-18 12:20:20 +0800

    feat: add cookie check modal.

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,19 @@
       <!-- footer -->
       <Footer />
     </v-main>
+    <!-- Cookie Check -->
+    <CookieCheck />
   </v-app>
 </template>
 
 <script>
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import CookieCheck from "@/components/home/CookieCheck";
 
 export default {
   name: "App",
-  components: { Header, Footer },
+  components: { Header, Footer, CookieCheck },
   data() {
     return {};
   },
